38711bde9f8055abf040c9bd413781982281d032
Md. Amzadullah Ratul <md.amzadullahratul@gamil.com>
2026-05-18 04:05:13 +0600

    first commit

---
+++ day1.py
@@ -0,0 +1,20 @@
+# # This is a simple Python program that prints "Hello, World!" to the console.
+# print("Hello, World!")
+# ## The following lines demonstrate how to print multiple lines and concatenate strings.
+# print("Hello, World!\n""Hello, World!\n""Hello, World!\n""Hello, World!\n")
+# # Concatenating strings using the '+' operator.
+# print("Hello"+" "+"Ratul")
+# # The following line takes user input and greets the user by name.
+# print("Hello"+" "+input("Enter your name: ")+"!")
+# #variables in Python
+# name = input("Enter your name: ")
+# print("Hello"+" "+name+"!")
+# #how to get the length of a string
+# name_length = len(name)
+# print("The length of your name is:", name_length)
+# #15. Day 1 Project Band Name Generator
+city = input("Which city did you grow up in? \n")
+pet = input("What is the name of your pet? \n")
+print("Your city is " + city)
+print("Your pet's name is " + pet)
+print("Your band name could be " + city + " " + pet)
